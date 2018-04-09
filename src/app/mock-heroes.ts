929c3972de9b6c7ab432d14822efc324e2183355
regbake <regbake@gmail.com>
2018-04-09 14:57:12 -0700

    styling etc

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Mr. Taco'},
+  { id: 42, name: 'Miss. Taco'},
+  { id: 13, name: 'Banana Hammock'},
+  { id: 2, name: 'Sailer Jherry'},
+  { id: 23, name: 'which one is pink'},
+  { id: 5, name: 'Heidi'},
+  { id: 4, name: 'surpressed laughter'}
+]
